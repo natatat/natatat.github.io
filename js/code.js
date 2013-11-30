02fde6158f13dab4f9dd3bb324bf45390acbcdd2
natatat <natuduwela@gmail.com>
2013-11-30 10:06:25 -0800

    hide others on I code things click

--- js/code.js
+++ js/code.js
@@ -1,5 +1,7 @@
 var Code = {
   show: function(){
+    $(".images.design").hide();
+    $(".images.built").hide();
     $("div.nav-code").slideToggle();
     var link = $("a.code");
     if ( link.hasClass("actived") ) {
