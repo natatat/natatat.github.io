92b090324e61bb34dbd560b4b6a5015a997f8ffd
natatat <natuduwela@gmail.com>
2015-03-11 13:20:08 -0700

    add compiled assets

---
+++ src/app.js
@@ -0,0 +1,23 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*global ga*/
+/*global $*/
+/*global window*/
+
+module.exports = {
+    run: function () {
+        if (this._prod()) {
+            ga('create', 'UA-36982620-1', 'auto');
+            ga('send', 'pageview');
+        }
+    },
+    _prod: function () {
+        "use strict";
+        if (window.location.href.indexOf('localhost') === -1) {
+            return true;
+        }
+        return false;
+    }
+};
+
+module.exports.run();
+},{}]},{},[1]);
